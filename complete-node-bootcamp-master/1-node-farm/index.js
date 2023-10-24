686f191dda0442ea1b66a233555585f739b3a083
aasthapit <aa.sthapit@live.com>
2023-10-24 12:08:25 -0700

    Read JSON and add route
    read JSON File
    Parse JSON into object
    Display object on the /api Endpoint

--- complete-node-bootcamp-master/1-node-farm/index.js
+++ complete-node-bootcamp-master/1-node-farm/index.js
@@ -3,6 +3,9 @@ const http = require('http')
 const path = require('path/posix')
 const url = require('url')
 
+
+//////////////////////////
+// // Read and write Files
 // //Blocking, syncronous way
 // const textIn = fs.readFileSync('./starter/txt/input.txt','utf-8')
 // console.log(textIn)
@@ -37,6 +40,14 @@ const server = http.createServer((req,res) => {
         res.end('This is the OVERVIEW')
     } else if (pathName === '/product'){
         res.end('This is the PRODUCT')
+    } else if (pathName === '/api'){
+
+        fs.readFile(`${__dirname}/starter/dev-data/data.json`, 'utf-8', (err, data) => {
+            const productData = JSON.parse(data)
+            res.writeHead(200, { 'Content-Type' : 'application/json'})
+            res.end(data)
+        })
+
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html'
