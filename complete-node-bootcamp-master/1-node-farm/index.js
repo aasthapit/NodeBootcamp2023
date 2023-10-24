b4cc2758fc2d97a0e31ad20e06ae080aa0bca206
aasthapit <aa.sthapit@live.com>
2023-10-24 14:12:49 -0700

    HTML Templating
    Create HTML from templates and replacing values from JSON
    
    Parse variables from URL for routing to pages
    
    Conditional logic for CSS classes to control rendering of "Organic" tag

--- complete-node-bootcamp-master/1-node-farm/index.js
+++ complete-node-bootcamp-master/1-node-farm/index.js
@@ -3,51 +3,58 @@ const http = require('http')
 const path = require('path/posix')
 const url = require('url')
 
-
-//////////////////////////
-// // Read and write Files
-// //Blocking, syncronous way
-// const textIn = fs.readFileSync('./starter/txt/input.txt','utf-8')
-// console.log(textIn)
-// const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on ${Date.now()}`
-// fs.writeFileSync('./starter/txt/output.txt',textOut)
-// console.log('File Written')
-
-
-// //Non-blocking, asynchronous way
-// fs.readFile('./starter/txt/start.txt','utf-8', (err, data1) => {
-//     if(err) return console.log('Error 🙅🏽')
-//     fs.readFile(`./starter/txt/${data1}.txt`,'utf-8', (err, data2) => {
-//         console.log(data2)    
-//         fs.readFile('./starter/txt/append.txt','utf-8', (err,data3) => {
-//             console.log(data3)    
-//             fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}` ,'utf-8', err => {
-//                 console.log('Your file has been written')    
-//             })
-//         })
-//     })
-// })
-
-// console.log('Will read File!')
-
 //////////////////////
 //SERVER
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%FROM%}/g, product.from)
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    output = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%DESCRIPTION%}/g, product.description)
+    output = output.replace(/{%ID%}/g, product.id)
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+
+    return output
+}
+
 //As thie file is being run once, we are able to run this syncronously to handle data. While it is blocking, since it runs once, this is okay
+const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`, 'utf-8')
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
-const server = http.createServer((req,res) => {
 
-    const pathName = req.url 
-    if(pathName === '/' || pathName === '/overview') {
-        res.end('This is the OVERVIEW')
-    } else if (pathName === '/product'){
-        res.end('This is the PRODUCT')
-    } else if (pathName === '/api'){
+const server = http.createServer((req,res) => {
+    const {query, pathname} = url.parse(req.url, true)
+    //Overview Page
+    if(pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, { 'Content-Type' : 'text/html'})
+
+        // for each el in dataObj, replace all placeholders as specified in replaceTemplate. As this returns as an array of values, to get a single string on HTML, join all elements in result array with null string
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
+
+        // replace placeholder of PRODUCTCARDS with resulting HTML string
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+        res.end(output)
+    } 
+    //Product page
+    else if (pathname === '/product'){
+        res.writeHead(200, { 'Content-Type' : 'text/html'})
+        const product = dataObj[query.id]
+        const output = replaceTemplate(tempProduct, product)
+        res.end(output)
+    } 
+    //API
+    else if (pathname === '/api'){
         res.writeHead(200, { 'Content-Type' : 'application/json'})
         res.end(data)
-    } else {
+    } 
+    //Not found
+    else {
         res.writeHead(404, {
             'Content-type': 'text/html'
         })
