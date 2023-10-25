c8865bead1e77b4b478ff09b489eb72d38ace6df
aasthapit <aa.sthapit@live.com>
2023-10-24 22:39:18 -0700

    NPM packages
    Using NPM we configured a NPM package and set nodemon as a dev dependency
    
    We can now start the application using npm start
    
    nodemon now allows for changes without having to manually restart the server

--- complete-node-bootcamp-master/1-node-farm/index.js
+++ complete-node-bootcamp-master/1-node-farm/index.js
@@ -3,6 +3,8 @@ const http = require('http')
 const path = require('path/posix')
 const url = require('url')
 
+const slugify = require('slugify')
+
 const replaceTemplate = require('./starter/modules/replaceTemplate')
 
 //////////////////////
@@ -15,6 +17,8 @@ const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-pro
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
+const slugs = dataObj.map(el => slugify(el.productName, {lower: true}))
+console.log(slugs)
 
 const server = http.createServer((req,res) => {
     const {query, pathname} = url.parse(req.url, true)
