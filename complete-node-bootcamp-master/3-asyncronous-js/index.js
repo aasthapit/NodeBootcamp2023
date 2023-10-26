bd4f6fe30423575a3ebfec65da19c275eb3cf335
aasthapit <aa.sthapit@live.com>
2023-10-26 11:58:07 -0700

    Correcting typos

--- complete-node-bootcamp-master/3-asyncronous-js/index.js
+++ complete-node-bootcamp-master/3-asyncronous-js/index.js
@@ -1,36 +1,35 @@
 const fs = require('fs');
-const { resolve } = require('path/posix');
 const superagent = require('superagent');
 
 const readFilePro = (file) => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, (err, data) => {
-      if (err) reject('I could not find that file 🫡');
+      if (err) reject('Could not find that file 😢');
       resolve(data);
     });
   });
 };
 
 const writeFilePro = (file, data) => {
-  return new Promise(resolve, (reject) => {
+  return new Promise((resolve, reject) => {
     fs.writeFile(file, data, (err) => {
-      if (err) reject('I could not write to that file 🫡');
+      if (err) reject('Could not write file 😢');
       resolve('success');
     });
   });
 };
 
-readFilePro(`${__dirname}/doggg.txt`)
+readFilePro(`${__dirname}/dog.txt`)
   .then((data) => {
     console.log(`Breed: ${data}`);
     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
   })
   .then((res) => {
     console.log(res.body.message);
-    return writeFilePro('dog-image.txt', res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message);
   })
   .then(() => {
-    console.log('Random dog image saved to file');
+    console.log('Random dog image saved to file!');
   })
   .catch((err) => {
     console.log(err);
