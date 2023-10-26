4c2458523297aedd3271496e7dca38d7cbffe4b0
aasthapit <aa.sthapit@live.com>
2023-10-26 10:04:35 -0700

    adding test-module3
    test with caching
    
    as node has caching, the module was only loaded once, so the code was only ran once
    
    The text of Log this text 🌻 was in cache, so when calling the function, the text was retreivied from cache

--- complete-node-bootcamp-master/2-how-node-works/modules.js
+++ complete-node-bootcamp-master/2-how-node-works/modules.js
@@ -6,6 +6,11 @@ const C = require('./test-module1');
 const calc1 = new C();
 console.log(calc1.add(3, 5));
 
-//exports
+// exports
 const { add, multiply, divide } = require('./test-module2');
 console.log(multiply(3, 5));
+
+// caching
+require('./test-module3')();
+require('./test-module3')();
+require('./test-module3')();
